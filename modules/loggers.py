d59a4f5330e3cc4ded9c5188d55dbbf164c99a21
Julianexe <julianssonko@gmail.com>
2025-03-18 18:24:22 +0300

    Add custom Logger implementation and create loggers module

---
+++ modules/loggers.py
@@ -0,0 +1,16 @@
+import logging
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
+    def get_logger(self):
+        return self.logger
+
