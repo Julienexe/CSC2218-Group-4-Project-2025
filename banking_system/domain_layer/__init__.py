ed0f8321a33ae84e83258887271e9563a5e54006
Julianexe <julianssonko@gmail.com>
2025-04-18 18:22:59 +0300

    delete old modules

--- banking_system/domain_layer/__init__.py
+++
@@ -1,4 +0,0 @@
-from banking_system.domain_layer.entities.bank_accounts.account import Account, AccountType
-from banking_system.domain_layer.entities.bank_accounts.checking_account import CheckingAccount
-from banking_system.domain_layer.entities.bank_accounts.savings_account import SavingsAccount
-from .entities.transaction import Transaction
