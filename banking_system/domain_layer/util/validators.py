14f04160b2bed579d05007a31b90b9049ec5bb8c
Julianexe <julianssonko@gmail.com>
2025-04-19 09:27:15 +0300

    Add transaction validation decorator and improve validator file formatting

--- banking_system/domain_layer/util/validators.py
+++ banking_system/domain_layer/util/validators.py
@@ -2,6 +2,7 @@
 This file holds the validator class that holds methods, all predicates for common validation issues that cut accross the account and transaction classes
 """
 
+
 #to keep things simple, use validator function for now
 #TODO when multiple functions are created, use one class to simplify imports 
 
@@ -13,3 +14,4 @@ def float_greater_than_zero(value:float) -> bool :
     else:
         print("Validation failed.")
         return False
+
